971854f6144b5d1fd0e29e2c12493cba2e3234b5
Andrew Fisher <andrewfishersb@gmail.com>
2016-08-12 14:14:00 -0700

    add a variable for css, android and ruby and many branches connected to questions which will increment the variables depending on the answer

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,84 @@ $(document).ready(function(){
       var pictureInput = $("input:radio[name=pic]:checked").val();
       var activityInput = $("input:radio[name=activity]:checked").val();
 
-      //may not need
-      var animalInput = $("input:radio[name=animal]:checked").val();
+      //q1
+      if(workTypeInput==='Web Applications'){
+        ruby++;
+      }else if(workTypeInput ==='Phone Applications'){
+        android++;
+      }else if(workTypeInput ==='Webpages'){
+        css++;
+      }
+
+      //q2
+      if(locationTypeInput==='A Start-up'){
+        ruby++;
+      }else if(locationTypeInput ==='A Big Business'){
+        android++;
+      }else if(locationTypeInput ==='From Home (Freelance)'){
+        css++;
+        android++;
+      }
 
 
+      //q3
+      if(companyInput==='airbnb'){
+        ruby++;
+      }else if(companyInput ==='netflix'){
+        android++;
+      }else if(companyInput ==='media'){
+        css++;
+      }
 
 
+      //q4
+      if(devTypeInput==='backend'){
+        ruby++;
+        android++;
+      }else if(devTypeInput ==='frontend'){
+        css++;
+      }else if(devTypeInput==='fullstack'){
+        ruby++;
+        android++;
+        css++;
+      }
 
 
+      //q5
+      if(frameworkInput==='Rails'){
+        ruby++;
+      }else if(frameworkInput ==='Bootstrap'){
+        css++;
+      }
 
+      //q6 - see if can change the background color
+      if(pictureInput==='london'){
+        $("body").css("background-color","green");
+      }else if(pictureInput==='sanfrancisco'){
+        $("body").css("background-color","pink");
+      }else if(pictureInput==='newyork'){
+        $("body").css("background-color","blue");
+      }
 
+      //q7
+      if(activityInput==='logical'){
+        ruby++;
+        android++;
+      }else if(activityInput ==='creative'){
+        css++;
+      }
+      alert("CSS: " + css);
+      alert("Java: " + android );
+      alert("Ruby: " + ruby);
+      //q8
+      var animal = Math.floor(Math.random()*4)+1;
+      if(animal===1){
+        ruby++;
+      }else if(animal ===2){
+        android++;
+      }else if(animal ===3){
+        css++;
+      }
+  
     });
-
-
-});
+  });
